0d2d5b71e27a127c52906b89f03bdc47671aab89
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-05-30 23:58:32 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_nums.append(num)
+    return even_nums if even_nums else []
 
 def make_exclamation(sentence_list):
-    pass
+    for i in range(len(sentence_list)):
+        if len(sentence_list) > 0:
+            sentence_list[i] += "!"
+    return sentence_list if sentence_list else []
